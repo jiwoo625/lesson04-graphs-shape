46e28fcb20a4a8e1795020ca7b0cd2542d05827a
jiwoo625 <jiwoo090625@naver.com>
2026-09-22 15:41:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 # ── 그래프 2. 장르 안의 영화 (트리맵) ──
-
 st.header("2. 장르 안의 영화 (트리맵)")
 
 fig2 = px.treemap(
-df,
-path=["장르", "movieNm"],
-values="total_audi",
+    df,
+    path=["장르", "movieNm"],
+    values="total_audi"
 )
 
 fig2.update_traces(
-textfont=dict(color="black"),
-hovertemplate=(
-"<b>%{label}</b><br>"
-"총 관객: %{value:,}명"
-"<extra></extra>"
-),
+    textfont=dict(color="black"),
+    hovertemplate="<b>%{label}</b><br>총 관객: %{value:,}명<extra></extra>"
 )
 
 st.plotly_chart(fig2, width="stretch")
 
-st.caption("이 그래프로 알 수 있는 것: (한 문장으로 적어 보세요)")
-
-st.divider()
+st.text_input("이 그래프로 알 수 있는 것", key="note2")
